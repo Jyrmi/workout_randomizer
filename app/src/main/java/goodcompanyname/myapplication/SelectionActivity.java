d3ad0a22938ce89136fe23279b3ef0ff676f2f6c
Jyrmi <jeremyschoi@gmail.com>
2016-08-15 09:12:47 -0400

    display muscle group in each cardview

--- app/src/main/java/goodcompanyname/myapplication/SelectionActivity.java
+++ app/src/main/java/goodcompanyname/myapplication/SelectionActivity.java
@@ -8,24 +8,31 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
+
 import com.android.volley.RequestQueue;
 
 import java.util.ArrayList;
 
 public class SelectionActivity extends AppCompatActivity {
 
-    ImageView imageAbs;
-    ImageView imageBase;
-    ImageView imageBiceps;
-    ImageView imageChest;
-    ImageView imageForearms;
-    ImageView imageFrontCalves;
-    ImageView imageFrontHamstrings;
-    ImageView imageFrontLats;
-    ImageView imageFrontShoulders;
-    ImageView imageFrontTraps;
-    ImageView imageNeck;
-    ImageView imageQuads;
+//    ImageView imageAbs;
+//    ImageView imageBase;
+//    ImageView imageBiceps;
+//    ImageView imageChest;
+//    ImageView imageForearms;
+//    ImageView imageFrontCalves;
+//    ImageView imageFrontHamstrings;
+//    ImageView imageFrontLats;
+//    ImageView imageFrontShoulders;
+//    ImageView imageFrontTraps;
+//    ImageView imageNeck;
+//    ImageView imageQuads;
+
+    ImageView imageGroupA;
+    ImageView imageGroupB;
+    ImageView imageGroupC;
+    ImageView imageGroupD;
 
     Button buttonAbsLower;
     Button buttonAbsUpper;
@@ -52,22 +59,31 @@ public class SelectionActivity extends AppCompatActivity {
         buttonGenerate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                navigateToWorkout();
+                if (selectedMuscleGroups.isEmpty()) {
+                    toastInvalidSelection();
+                } else {
+                    navigateToWorkout();
+                }
             }
         });
 
-        imageAbs = (ImageView) findViewById(R.id.image_abs);
-        imageBase = (ImageView) findViewById(R.id.image_base);
-        imageBiceps = (ImageView) findViewById(R.id.image_biceps);
-        imageChest = (ImageView) findViewById(R.id.image_chest);
-        imageForearms = (ImageView) findViewById(R.id.image_forearms);
-        imageFrontCalves = (ImageView) findViewById(R.id.image_front_calves);
-        imageFrontHamstrings = (ImageView) findViewById(R.id.image_front_hamstrings);
-        imageFrontLats = (ImageView) findViewById(R.id.image_front_lats);
-        imageFrontShoulders = (ImageView) findViewById(R.id.image_front_shoulders);
-        imageFrontTraps = (ImageView) findViewById(R.id.image_front_traps);
-        imageNeck = (ImageView) findViewById(R.id.image_neck);
-        imageQuads = (ImageView) findViewById(R.id.image_quads);
+//        imageAbs = (ImageView) findViewById(R.id.image_abs);
+//        imageBase = (ImageView) findViewById(R.id.image_base);
+//        imageBiceps = (ImageView) findViewById(R.id.image_biceps);
+//        imageChest = (ImageView) findViewById(R.id.image_chest);
+//        imageForearms = (ImageView) findViewById(R.id.image_forearms);
+//        imageFrontCalves = (ImageView) findViewById(R.id.image_front_calves);
+//        imageFrontHamstrings = (ImageView) findViewById(R.id.image_front_hamstrings);
+//        imageFrontLats = (ImageView) findViewById(R.id.image_front_lats);
+//        imageFrontShoulders = (ImageView) findViewById(R.id.image_front_shoulders);
+//        imageFrontTraps = (ImageView) findViewById(R.id.image_front_traps);
+//        imageNeck = (ImageView) findViewById(R.id.image_neck);
+//        imageQuads = (ImageView) findViewById(R.id.image_quads);
+
+        imageGroupA = (ImageView) findViewById(R.id.image_group_a);
+        imageGroupB = (ImageView) findViewById(R.id.image_group_b);
+        imageGroupC = (ImageView) findViewById(R.id.image_group_c);
+        imageGroupD = (ImageView) findViewById(R.id.image_group_d);
 
         buttonAbsLower = (Button) findViewById(R.id.button_abs_lower);
         buttonAbsUpper = (Button) findViewById(R.id.button_abs_upper);
@@ -84,58 +100,177 @@ public class SelectionActivity extends AppCompatActivity {
         buttonQuads = (Button) findViewById(R.id.button_quads);
 
         selectedMuscleGroups = new ArrayList<MuscleGroup>();
-        setButtonListener(buttonAbsLower, MuscleGroup.ABS, imageAbs,
-                R.drawable.abs, R.drawable.abs_colorized);
-        setButtonListener(buttonAbsUpper, MuscleGroup.ABS, imageAbs,
-                R.drawable.abs, R.drawable.abs_colorized);
-        setButtonListener(buttonBiceps, MuscleGroup.BICEPS, imageBiceps,
-                R.drawable.biceps, R.drawable.biceps_colorized);
-        setButtonListener(buttonChest, MuscleGroup.CHEST, imageChest,
-                R.drawable.chest, R.drawable.chest_colorized);
-        setButtonListener(buttonForearms, MuscleGroup.FOREARMS, imageForearms,
-                R.drawable.forearms, R.drawable.forearms_colorized);
-        setButtonListener(buttonFrontCalves, MuscleGroup.CALVES, imageFrontCalves,
-                R.drawable.front_calves, R.drawable.front_calves_colorized);
-        setButtonListener(buttonFrontHamstrings, MuscleGroup.HAMSTRINGS, imageFrontHamstrings,
-                R.drawable.front_hamstrings, R.drawable.front_hamstrings_colorized);
-        setButtonListener(buttonFrontLatsLeft, MuscleGroup.LATS, imageFrontLats,
-                R.drawable.front_lats, R.drawable.front_lats_colorized);
-        setButtonListener(buttonFrontLatsRight, MuscleGroup.LATS, imageFrontLats,
-                R.drawable.front_lats, R.drawable.front_lats_colorized);
-        setButtonListener(buttonFrontShoulders, MuscleGroup.SHOULDERS, imageFrontShoulders,
-                R.drawable.front_shoulders, R.drawable.front_shoulders_colorized);
-        setButtonListener(buttonFrontTraps, MuscleGroup.TRAPS, imageFrontTraps,
-                R.drawable.front_traps, R.drawable.front_traps_colorized);
-        setButtonListener(buttonNeck, MuscleGroup.NECK, imageNeck,
-                R.drawable.neck, R.drawable.neck_colorized);
-        setButtonListener(buttonQuads, MuscleGroup.QUADS, imageQuads,
-                R.drawable.quads, R.drawable.quads_colorized);
+
+        setButtonListener(buttonAbsLower, MuscleGroup.ABS, imageGroupC);
+        setButtonListener(buttonAbsUpper, MuscleGroup.ABS, imageGroupC);
+        setButtonListener(buttonBiceps, MuscleGroup.BICEPS, imageGroupB);
+        setButtonListener(buttonChest, MuscleGroup.CHEST, imageGroupA);
+        setButtonListener(buttonForearms, MuscleGroup.FOREARMS, imageGroupB);
+        setButtonListener(buttonFrontCalves, MuscleGroup.CALVES, imageGroupD);
+        setButtonListener(buttonFrontHamstrings, MuscleGroup.HAMSTRINGS, imageGroupC);
+        setButtonListener(buttonFrontLatsLeft, MuscleGroup.LATS, imageGroupC);
+        setButtonListener(buttonFrontLatsRight, MuscleGroup.LATS, imageGroupC);
+        setButtonListener(buttonFrontShoulders, MuscleGroup.SHOULDERS, imageGroupB);
+        setButtonListener(buttonFrontTraps, MuscleGroup.TRAPS, imageGroupA);
+        setButtonListener(buttonNeck, MuscleGroup.NECK, imageGroupA);
+        setButtonListener(buttonQuads, MuscleGroup.QUADS, imageGroupD);
+
+//        setButtonListener(buttonAbsLower, MuscleGroup.ABS, imageAbs,
+//                R.drawable.abs, R.drawable.abs_colorized);
+//        setButtonListener(buttonAbsUpper, MuscleGroup.ABS, imageAbs,
+//                R.drawable.abs, R.drawable.abs_colorized);
+//        setButtonListener(buttonBiceps, MuscleGroup.BICEPS, imageBiceps,
+//                R.drawable.biceps, R.drawable.biceps_colorized);
+//        setButtonListener(buttonChest, MuscleGroup.CHEST, imageChest,
+//                R.drawable.chest, R.drawable.chest_colorized);
+//        setButtonListener(buttonForearms, MuscleGroup.FOREARMS, imageForearms,
+//                R.drawable.forearms, R.drawable.forearms_colorized);
+//        setButtonListener(buttonFrontCalves, MuscleGroup.CALVES, imageFrontCalves,
+//                R.drawable.front_calves, R.drawable.front_calves_colorized);
+//        setButtonListener(buttonFrontHamstrings, MuscleGroup.HAMSTRINGS, imageFrontHamstrings,
+//                R.drawable.front_hamstrings, R.drawable.front_hamstrings_colorized);
+//        setButtonListener(buttonFrontLatsLeft, MuscleGroup.LATS, imageFrontLats,
+//                R.drawable.front_lats, R.drawable.front_lats_colorized);
+//        setButtonListener(buttonFrontLatsRight, MuscleGroup.LATS, imageFrontLats,
+//                R.drawable.front_lats, R.drawable.front_lats_colorized);
+//        setButtonListener(buttonFrontShoulders, MuscleGroup.SHOULDERS, imageFrontShoulders,
+//                R.drawable.front_shoulders, R.drawable.front_shoulders_colorized);
+//        setButtonListener(buttonFrontTraps, MuscleGroup.TRAPS, imageFrontTraps,
+//                R.drawable.front_traps, R.drawable.front_traps_colorized);
+//        setButtonListener(buttonNeck, MuscleGroup.NECK, imageNeck,
+//                R.drawable.neck, R.drawable.neck_colorized);
+//        setButtonListener(buttonQuads, MuscleGroup.QUADS, imageQuads,
+//                R.drawable.quads, R.drawable.quads_colorized);
+    }
+
+    private void setGroupImageResource(ImageView imageView) {
+        if (imageView == imageGroupA) {
+            if (selectedMuscleGroups.contains(MuscleGroup.NECK) &&
+                    selectedMuscleGroups.contains(MuscleGroup.TRAPS) &&
+                    selectedMuscleGroups.contains(MuscleGroup.CHEST)) {
+                imageView.setImageResource(R.drawable.a_all);
+            } else if (selectedMuscleGroups.contains(MuscleGroup.NECK) &&
+                    selectedMuscleGroups.contains(MuscleGroup.TRAPS)) {
+                imageView.setImageResource(R.drawable.a_neck_traps);
+            } else if (selectedMuscleGroups.contains(MuscleGroup.NECK) &&
+                    selectedMuscleGroups.contains(MuscleGroup.CHEST)) {
+                imageView.setImageResource(R.drawable.a_neck_chest);
+            } else if (selectedMuscleGroups.contains(MuscleGroup.TRAPS) &&
+                    selectedMuscleGroups.contains(MuscleGroup.CHEST)) {
+                imageView.setImageResource(R.drawable.a_traps_chest);
+            } else if (selectedMuscleGroups.contains(MuscleGroup.NECK)) {
+                imageView.setImageResource(R.drawable.a_neck);
+            } else if (selectedMuscleGroups.contains(MuscleGroup.TRAPS)) {
+                imageView.setImageResource(R.drawable.a_traps);
+            } else if (selectedMuscleGroups.contains(MuscleGroup.CHEST)) {
+                imageView.setImageResource(R.drawable.a_chest);
+            } else {
+                imageView.setImageResource(R.drawable.a_none);
+            }
+        } else if (imageView == imageGroupB) {
+            if (selectedMuscleGroups.contains(MuscleGroup.SHOULDERS) &&
+                    selectedMuscleGroups.contains(MuscleGroup.BICEPS) &&
+                    selectedMuscleGroups.contains(MuscleGroup.FOREARMS)) {
+                imageView.setImageResource(R.drawable.b_all);
+            } else if (selectedMuscleGroups.contains(MuscleGroup.SHOULDERS) &&
+                    selectedMuscleGroups.contains(MuscleGroup.BICEPS)) {
+                imageView.setImageResource(R.drawable.b_shoulders_biceps);
+            } else if (selectedMuscleGroups.contains(MuscleGroup.SHOULDERS) &&
+                    selectedMuscleGroups.contains(MuscleGroup.FOREARMS)) {
+                imageView.setImageResource(R.drawable.b_shoulders_forearms);
+            } else if (selectedMuscleGroups.contains(MuscleGroup.BICEPS) &&
+                    selectedMuscleGroups.contains(MuscleGroup.FOREARMS)) {
+                imageView.setImageResource(R.drawable.b_biceps_forearms);
+            } else if (selectedMuscleGroups.contains(MuscleGroup.SHOULDERS)) {
+                imageView.setImageResource(R.drawable.b_shoulders);
+            } else if (selectedMuscleGroups.contains(MuscleGroup.BICEPS)) {
+                imageView.setImageResource(R.drawable.b_biceps);
+            } else if (selectedMuscleGroups.contains(MuscleGroup.FOREARMS)) {
+                imageView.setImageResource(R.drawable.b_forearms);
+            } else {
+                imageView.setImageResource(R.drawable.b_none);
+            }
+        } else if (imageView == imageGroupC) {
+            if (selectedMuscleGroups.contains(MuscleGroup.LATS) &&
+                    selectedMuscleGroups.contains(MuscleGroup.ABS) &&
+                    selectedMuscleGroups.contains(MuscleGroup.HAMSTRINGS)) {
+                imageView.setImageResource(R.drawable.c_all);
+            } else if (selectedMuscleGroups.contains(MuscleGroup.LATS) &&
+                    selectedMuscleGroups.contains(MuscleGroup.ABS)) {
+                imageView.setImageResource(R.drawable.c_lats_abs);
+            } else if (selectedMuscleGroups.contains(MuscleGroup.LATS) &&
+                    selectedMuscleGroups.contains(MuscleGroup.HAMSTRINGS)) {
+                imageView.setImageResource(R.drawable.c_lats_hamstrings);
+            } else if (selectedMuscleGroups.contains(MuscleGroup.ABS) &&
+                    selectedMuscleGroups.contains(MuscleGroup.HAMSTRINGS)) {
+                imageView.setImageResource(R.drawable.c_abs_hamstrings);
+            } else if (selectedMuscleGroups.contains(MuscleGroup.LATS)) {
+                imageView.setImageResource(R.drawable.c_lats);
+            } else if (selectedMuscleGroups.contains(MuscleGroup.ABS)) {
+                imageView.setImageResource(R.drawable.c_abs);
+            } else if (selectedMuscleGroups.contains(MuscleGroup.HAMSTRINGS)) {
+                imageView.setImageResource(R.drawable.c_hamstrings);
+            } else {
+                imageView.setImageResource(R.drawable.c_none);
+            }
+        } else if (imageView == imageGroupD) {
+            if (selectedMuscleGroups.contains(MuscleGroup.QUADS) &&
+                    selectedMuscleGroups.contains(MuscleGroup.CALVES)) {
+                imageView.setImageResource(R.drawable.d_all);
+            } else if (selectedMuscleGroups.contains(MuscleGroup.QUADS)) {
+                imageView.setImageResource(R.drawable.d_quads);
+            } else if (selectedMuscleGroups.contains(MuscleGroup.CALVES)) {
+                imageView.setImageResource(R.drawable.d_calves);
+            } else {
+                imageView.setImageResource(R.drawable.d_none);
+            }
+        }
     }
 
     /**
      * Sets listeners for this activity's buttons.
      * @param button the button.
      * @param imageView the ImageView associated with the button.
-     * @param imageViewAssetoffResId the resource id (ex. R.drawable.abs resolves to an int) of the
-     *                               image resource in it's 'off' state.
-     * @param imageViewAssetOnResId opposite of above.
      */
-    private void setButtonListener(Button button, final MuscleGroup muscleGroup,
-                                   final ImageView imageView, final int imageViewAssetoffResId,
-                                   final int imageViewAssetOnResId) {
+    private void setButtonListener(Button button, final MuscleGroup muscleGroup, final ImageView imageView) {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (selectedMuscleGroups.contains(muscleGroup)) {
                     selectedMuscleGroups.remove(muscleGroup);
-                    imageView.setImageResource(imageViewAssetoffResId);
                 } else {
                     selectedMuscleGroups.add(muscleGroup);
-                    imageView.setImageResource(imageViewAssetOnResId);
                 }
+
+                setGroupImageResource(imageView);
             }
         });
     }
+
+//    /**
+//     * Sets listeners for this activity's buttons.
+//     * @param button the button.
+//     * @param imageView the ImageView associated with the button.
+//     * @param imageViewAssetoffResId the resource id (ex. R.drawable.abs resolves to an int) of the
+//     *                               image resource in it's 'off' state.
+//     * @param imageViewAssetOnResId opposite of above.
+//     */
+//    private void setButtonListener(Button button, final MuscleGroup muscleGroup,
+//                                   final ImageView imageView, final int imageViewAssetoffResId,
+//                                   final int imageViewAssetOnResId) {
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                if (selectedMuscleGroups.contains(muscleGroup)) {
+//                    selectedMuscleGroups.remove(muscleGroup);
+//                    imageView.setImageResource(imageViewAssetoffResId);
+//                } else {
+//                    selectedMuscleGroups.add(muscleGroup);
+//                    imageView.setImageResource(imageViewAssetOnResId);
+//                }
+//            }
+//        });
+//    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -160,11 +295,15 @@ public class SelectionActivity extends AppCompatActivity {
     }
 
     /** Called when the user clicks the Send button */
+    public void toastInvalidSelection() {
+        Toast.makeText(SelectionActivity.this, R.string.invalid_selection, Toast.LENGTH_SHORT).show();
+    }
+
+    /** Called when the user clicks the Send button */
     public void navigateToWorkout() {
         // Do something in response to button
         Intent intent = new Intent(this, WorkoutActivity.class);
         intent.putExtra("EXTRA_MUSCLE_GROUPS", selectedMuscleGroups);
         startActivity(intent);
-        finish();
     }
 }
--- app/src/main/java/goodcompanyname/myapplication/WorkoutActivity.java
+++ app/src/main/java/goodcompanyname/myapplication/WorkoutActivity.java
@@ -1,18 +1,23 @@
 package goodcompanyname.myapplication;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -21,24 +26,40 @@ public class WorkoutActivity extends AppCompatActivity {
     private static final String TAG = "WorkoutActivity";
 
     private Button buttonDone;
-    private ArrayList<String> exercisesList;
-    private ArrayAdapter<String> exercisesAdapter;
-    private ArrayList<MuscleGroup> muscleGroups;
     private HashMap<MuscleGroup, ArrayList<String>> exercises;
+    private ArrayList<MuscleGroup> selectedMuscleGroups;
+    private HashMap<MuscleGroup, String> muscleGroupNames;
+    private ListView listViewExercises;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_workout);
 
-        exercisesList = new ArrayList<String>();
-
         Intent intent = getIntent();
         Bundle bundle = intent.getExtras();
-        muscleGroups = (ArrayList<MuscleGroup>) bundle.get("EXTRA_MUSCLE_GROUPS");
+        selectedMuscleGroups = (ArrayList<MuscleGroup>) bundle.get("EXTRA_MUSCLE_GROUPS");
 
         // DEBUG
-        Log.d(TAG, muscleGroups.toString());
+        Log.d(TAG, selectedMuscleGroups.toString());
+
+        muscleGroupNames = new HashMap<>();
+
+        muscleGroupNames.put(MuscleGroup.ABS, "Abs");
+        muscleGroupNames.put(MuscleGroup.BICEPS, "Biceps");
+        muscleGroupNames.put(MuscleGroup.CHEST, "Chest");
+        muscleGroupNames.put(MuscleGroup.FOREARMS, "Forearms");
+        muscleGroupNames.put(MuscleGroup.CALVES, "Calves");
+        muscleGroupNames.put(MuscleGroup.GLUTES, "Glutes");
+        muscleGroupNames.put(MuscleGroup.HAMSTRINGS, "Hamstrings");
+        muscleGroupNames.put(MuscleGroup.LATS, "Lats");
+        muscleGroupNames.put(MuscleGroup.LOWER_BACK, "Lower Back");
+        muscleGroupNames.put(MuscleGroup.MIDDLE_BACK, "Middle Back");
+        muscleGroupNames.put(MuscleGroup.SHOULDERS, "Shoulders");
+        muscleGroupNames.put(MuscleGroup.TRAPS, "Traps");
+        muscleGroupNames.put(MuscleGroup.TRICEPS, "Triceps");
+        muscleGroupNames.put(MuscleGroup.NECK, "Neck");
+        muscleGroupNames.put(MuscleGroup.QUADS, "Quads");
 
         ArrayList<String> neckExercises = new ArrayList<>();
         neckExercises.add("LYING FACE DOWN PLATE NECK RESISTANCE");
@@ -117,36 +138,40 @@ public class WorkoutActivity extends AppCompatActivity {
         exercises.put(MuscleGroup.GLUTES, glutesExercises);
         exercises.put(MuscleGroup.HAMSTRINGS, hamstringsExercises);
 
-        for (MuscleGroup muscleGroup : muscleGroups) {
+        ArrayList<HashMap<String, String>> groupExercisePairs = new ArrayList<>();
+
+        for (MuscleGroup muscleGroup : selectedMuscleGroups) {
             ArrayList<String> exerciseGroup = exercises.get(muscleGroup);
             if (exerciseGroup != null) {
                 for (String exercise : exerciseGroup) {
-                    exercisesList.add(exercise);
+                    HashMap<String, String> groupExercisePair = new HashMap<>();
+                    groupExercisePair.put("muscleGroup", muscleGroupNames.get(muscleGroup));
+                    groupExercisePair.put("exercise", exercise);
+                    groupExercisePairs.add(groupExercisePair);
                 }
             }
         }
 
-        exercisesAdapter = new ArrayAdapter<String>(this, R.layout.card_view,
-                R.id.list_exercises_item, exercisesList);
-        final ListView listViewExercises = (ListView) findViewById(R.id.exercises_list_view);
-        listViewExercises.setAdapter(exercisesAdapter);
+        ExerciseAdapter exerciseAdapter = new ExerciseAdapter(this, groupExercisePairs);
+
+        listViewExercises = (ListView) findViewById(R.id.exercises_list_view);
+        listViewExercises.setAdapter(exerciseAdapter);
 
         buttonDone = (Button) findViewById(R.id.button_done);
         buttonDone.setOnClickListener(new View.OnClickListener() {
-            ArrayList<String> checkedExercises = new ArrayList<String>();
+            ArrayList<String> checkedExercises = new ArrayList<>();
 
             @Override
             public void onClick(View view) {
-                // DEBUG
-                int i = 0;
+                int i;
                 for (i = 0; i < listViewExercises.getChildCount(); i++) {
                     View cardView = listViewExercises.getChildAt(i);
                     CheckBox checkBox =
-                            (CheckBox) cardView.findViewById(R.id.list_exercises_item_checkbox);
+                            (CheckBox) cardView.findViewById(R.id.list_view_checkbox);
 
                     if (checkBox.isChecked()) {
                         TextView textViewExercise =
-                                (TextView) cardView.findViewById(R.id.list_exercises_item);
+                                (TextView) cardView.findViewById(R.id.list_view_exercise);
                         checkedExercises.add(textViewExercise.getText().toString());
                         Log.d(TAG, textViewExercise.getText().toString());
                     }
@@ -159,6 +184,56 @@ public class WorkoutActivity extends AppCompatActivity {
                 finish();
             }
         });
+    }
+
+    private class ExerciseAdapter extends BaseAdapter {
+        private ArrayList<HashMap<String, String>> groupExercisePairs;
+        Activity activity;
+        TextView exercise;
+        TextView muscleGroup;
+
+        public ExerciseAdapter(Activity activity, ArrayList<HashMap<String, String>> list) {
+            super();
+            this.activity = activity;
+            this.groupExercisePairs = list;
+        }
+
+        @Override
+        public int getCount() {
+            // TODO Auto-generated method stub
+            return groupExercisePairs.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            // TODO Auto-generated method stub
+            return groupExercisePairs.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            // TODO Auto-generated method stub
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View cardView, ViewGroup parent) {
+            // TODO Auto-generated method stub
+            LayoutInflater inflater = activity.getLayoutInflater();
+
+            if (cardView == null) {
+                cardView = inflater.inflate(R.layout.card_view, null);
+
+                muscleGroup = (TextView) cardView.findViewById(R.id.list_view_muscle_group);
+                exercise = (TextView) cardView.findViewById(R.id.list_view_exercise);
+            }
+
+            HashMap<String, String> groupExercisePair = groupExercisePairs.get(position);
+            muscleGroup.setText(groupExercisePair.get("muscleGroup"));
+            exercise.setText(groupExercisePair.get("exercise"));
+
+            return cardView;
+        }
     }
 
     @Override
@@ -183,6 +258,3 @@ public class WorkoutActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 }
-
-
-
